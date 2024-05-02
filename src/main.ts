ca1d02a77b2706aba8935bae1dca2d58fce334f9
Bashar Gh <bashar.gh.dev@gmail.com>
2024-05-03 01:55:05 +0400

    Add winstonjs logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import { format, transports } from 'winston';
+
+const { File, Console } = transports;
+const { combine, timestamp, prettyPrint } = format;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new Console({
+          format: combine(timestamp(), prettyPrint({ colorize: true })),
+        }),
+        new File({
+          filename: 'combined.log',
+          level: 'debug',
+        }),
+      ],
+    }),
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
